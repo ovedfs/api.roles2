2191ba5f94b80161c7880b046b21264c5ac8e09f
ROP <photocromatic@gmail.com>
2022-10-27 02:30:40 -0500

    Agregamos la funcionalidad CRUD de los Posts asignada a los Roles: user, editor, admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Notifications\Notifiable;
+use App\Models\Post;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    function posts() {
+        return $this -> hasMany( Post::class );
+    }
 }
